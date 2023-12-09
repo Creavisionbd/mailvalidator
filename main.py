a129beaa48f683f4fa66fd58ac7227df69e75dfe
Creavisionbd <142791531+Creavisionbd@users.noreply.github.com>
2023-12-09 13:19:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -45,14 +45,14 @@ def process_csv(input_file):
     if input_file:
         if isinstance(input_file, str):  # For Streamlit sharing compatibility
             file_data = BytesIO(input_file.read())
-            with open(file_data, 'rb') as f:
+            with open(BytesIO(file_data.getvalue()), 'rb') as f:
                 result = chardet.detect(f.read())
 
             df = pd.read_csv(file_data, encoding=result['encoding'])
             # df = pd.read_csv(input_file,'rb')
         else:
             file_data = BytesIO(input_file.read())
-            with open(file_data, 'rb') as f:
+            with open(BytesIO(file_data.getvalue()), 'rb') as f:
                 result = chardet.detect(f.read())
 
             df = pd.read_csv(input_file, encoding=result['encoding'])
