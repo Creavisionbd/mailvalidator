8ae61791d74606c90a1f1683cbf6f3f464938671
Creavisionbd <142791531+Creavisionbd@users.noreply.github.com>
2023-12-09 12:59:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -43,9 +43,17 @@ def process_csv(input_file):
     # Read the uploaded file as a DataFrame
     if input_file:
         if isinstance(input_file, str):  # For Streamlit sharing compatibility
-            df = pd.read_csv(input_file,'rb')
+            with open('file.csv', 'rb') as f:
+                result = chardet.detect(f.read())
+
+            df = pd.read_csv('file.csv', encoding=result['encoding'])
+            # df = pd.read_csv(input_file,'rb')
         else:
-            df = pd.read_csv(input_file,'rb')
+            with open('file.csv', 'rb') as f:
+                result = chardet.detect(f.read())
+
+            df = pd.read_csv('file.csv', encoding=result['encoding'])
+            # df = pd.read_csv(input_file,'rb')
         
         # Create a list to store the results
         results = []
