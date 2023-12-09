92484a00c5a5d969f9ce709402a27236fcd2938c
Creavisionbd <142791531+Creavisionbd@users.noreply.github.com>
2023-12-09 14:11:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -45,11 +45,11 @@ def process_csv(input_file):
     # Read the uploaded file as a DataFrame
     if input_file:
         if isinstance(input_file, str):  # For Streamlit sharing compatibility
-            df = pd.read_csv(input_file,'rb')
+            df = pd.read_csv(input_file)
         else:
             file_data = BytesIO(input_file.read())
            
-            df = pd.read_csv(input_file,'rb')
+            df = pd.read_csv(input_file)
         
         # Create a list to store the results
         results = []
